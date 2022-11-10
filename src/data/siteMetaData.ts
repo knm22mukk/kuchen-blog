120da19af6dbd31b36d989d5b535fd7331d8f0ca
knm22mukk <knm22mukk@gmail.com>
2022-11-10 23:21:11 +0900

    ドメインを取得したのでサイトのURLを変更しました。

--- src/data/siteMetaData.ts
+++ src/data/siteMetaData.ts
@@ -2,7 +2,7 @@ export const siteMetaData = {
   title: 'くーへんブログ',
   description:
     'サラリーマンくーへんが仕事のこと、日常のこと、グルメのこと、おすすめ商品のことなどを気ままに発信する個人ブログです。',
-  siteUrl: 'https://kuchen-blog.vercel.app',
-  siteLogo: 'https://kuchen-blog.vercel.app/images/logo500x180.png',
+  siteUrl: 'https://kuchen-blog.net/',
+  siteLogo: 'https://kuchen-blog.net/images/logo500x180.png',
   twitter: 'kuchen_22',
 };
