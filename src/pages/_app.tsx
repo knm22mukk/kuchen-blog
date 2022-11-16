49335094e82aaf3bf76f4296d6df98a423ec1445
knm22mukk <knm22mukk@gmail.com>
2022-11-16 23:25:02 +0900

    GA4とGTMの設定を追加しました。

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import '../styles/globals.css';
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
+import GoogleTagManager, { GoogleTagManagerId } from '@/components/GoogleTagManager';
+import { googleTagManagerId } from '@/libs/gtm';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute='class' defaultTheme='light'>
+      <GoogleTagManager googleTagManagerId={googleTagManagerId as GoogleTagManagerId} />
       <Component {...pageProps} />
     </ThemeProvider>
   );
