96507fac29e6635b38644b49995b6ee1bf33d7f4
knm22mukk <knm22mukk@gmail.com>
2022-11-24 23:46:55 +0900

    ブログの本文のスタイルを調整しました。不要なcssファイルは削除しました。

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,52 @@ module.exports = {
           xl: '5rem',
         },
       },
+      typography: {
+        DEFAULT: {
+          css: {
+            h2: {
+              background: '#4F46E5',
+              borderRadius: '8px',
+              padding: '16px',
+              fontFamily: 'Inter',
+              fontStyle: 'normal',
+              fontWeight: '800',
+              fontSize: '36px',
+              lineHeight: '40px',
+              color: '#FFFFFF',
+            },
+
+            h3: {
+              borderLeft: '8px solid #4F46E5',
+              padding: '16px',
+              fontFamily: 'Inter',
+              fontStyle: 'normal',
+              fontWeight: '800',
+              fontSize: '28px',
+              lineHeight: '32px',
+            },
+
+            p: {
+              fontFamily: 'Inter',
+              fontStyle: 'normal',
+              fontWeight: 'normal',
+              fontSize: '16px',
+              lineHeight: '150%',
+            },
+
+            a: {
+              color: '#3182ce',
+              '&:hover': {
+                color: '#2c5282',
+              },
+            },
+
+            iframe: {
+              width: '100%',
+            },
+          },
+        },
+      },
     },
   },
   plugins: [require('@tailwindcss/typography')],
